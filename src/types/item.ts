4694c3c08a88bd77615437a07bf1cc28c0347160
Cory Sougstad <cory@hau.me>
2022-12-01 17:12:58 -0700

    Fix prettier format

--- src/types/item.ts
+++ src/types/item.ts
@@ -1,52 +1,52 @@
 export interface ItemRequestResponse {
-    item: Item;
+  item: Item;
 }
 
 export interface Item {
-    name:                 string;
-    description:          string;
-    properties:           ItemProperty[];
-    unlockableContent:    string;
-    tokenId:              number;
-    collectionAddress:    string;
-    chainId:              number;
-    royalty:              number;
-    creatorAddress:       string;
-    mintDate:             Date;
-    stackable:            boolean;
-    supply:               number;
-    maxSupply:            number;
-    image:                string;
-    imageHash:            string;
-    imageUrl:             string;
-    fileType:             string;
-    isVideo:              boolean;
-    allImages:            AllImage[];
-    otherImageVisibility: string;
-    holders:              number;
-    listers:              number;
-    likes:                number;
-    views:                number;
-    isListed:             boolean;
-    nsfw:                 boolean;
-    isVerified:           boolean;
-    hasUnlockableContent: boolean;
-    didLike:              boolean;
-    imageIndex:           number;
-    imageCount:           number;
+  name: string;
+  description: string;
+  properties: ItemProperty[];
+  unlockableContent: string;
+  tokenId: number;
+  collectionAddress: string;
+  chainId: number;
+  royalty: number;
+  creatorAddress: string;
+  mintDate: Date;
+  stackable: boolean;
+  supply: number;
+  maxSupply: number;
+  image: string;
+  imageHash: string;
+  imageUrl: string;
+  fileType: string;
+  isVideo: boolean;
+  allImages: AllImage[];
+  otherImageVisibility: string;
+  holders: number;
+  listers: number;
+  likes: number;
+  views: number;
+  isListed: boolean;
+  nsfw: boolean;
+  isVerified: boolean;
+  hasUnlockableContent: boolean;
+  didLike: boolean;
+  imageIndex: number;
+  imageCount: number;
 }
 
 export interface AllImage {
-    _id:       string;
-    imageHash: string;
-    image:     string;
-    fileType:  string;
-    isVideo:   boolean;
+  _id: string;
+  imageHash: string;
+  image: string;
+  fileType: string;
+  isVideo: boolean;
 }
 
 export interface ItemProperty {
-    _id?:    string;
-    name:   string;
-    value:  string;
-    static: boolean;
+  _id?: string;
+  name: string;
+  value: string;
+  static: boolean;
 }
