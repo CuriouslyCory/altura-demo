975fd32ebd3fbce788a5592cde4e0c2965e308e5
Cory Sougstad <cory@hau.me>
2022-12-01 17:13:34 -0700

    Add character details menu

--- src/features/layout/components/header.tsx
+++ src/features/layout/components/header.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import { LogoutButton } from "../../auth/components/logout-button";
+import { useAuthContext } from "../../auth/hooks/use-auth-context";
+import { CharacterDetails } from "../../character/components/character-details";
 
 export const Header = (): JSX.Element => {
+  const { isAuthenticated } = useAuthContext();
   return (
-    <header className="flex justify-between px-5 pt-5">
-      <Link href="/">Home</Link>
+    <header className="flex px-5 pt-5">
+      <Link href="/" className="mr-auto">
+        <button className="btn">Home</button>
+      </Link>
+      {isAuthenticated && <CharacterDetails />}
       <LogoutButton />
     </header>
   );
