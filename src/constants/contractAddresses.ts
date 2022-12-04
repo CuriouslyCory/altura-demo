5d33f4a6041d89c6d460247a4b5cc9b94d502df2
Cory Sougstad <cory@hau.me>
2022-12-03 18:15:49 -0700

    Add contract address constant

---
+++ src/constants/contractAddresses.ts
@@ -0,0 +1,7 @@
+// owned by stream wallet - 0x135ccb6ea490fff175d6b655d328b16fc0389a63
+// owned by dev wallet- 0x14c85b46fda7f4659306d6b7531d941dc9310fa3
+export const contractAddresses = {
+  5: {
+    abilityCollection: "0x14c85b46fda7f4659306d6b7531d941dc9310fa3",
+  },
+};
