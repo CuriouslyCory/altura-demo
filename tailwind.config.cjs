848e355e89ac548e207aa810a1752b2fe7e51097
Cory Sougstad <cory@hau.me>
2022-12-15 22:45:18 -0700

    Add dark theme Working on nested css compatibility

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("tailwindcss/nesting")],
 };
